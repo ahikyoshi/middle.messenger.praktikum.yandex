d8f10c717cbcd2934fece5d3f442bcaf8682277e
ahikyoshi <115409269+ahikyoshi@users.noreply.github.com>
2023-04-06 12:52:25 +0300

    chatList avatars
    Исправлено: Скрин https://disk.yandex.ru/i/WKsqQvMZTVBKqQ В моих чатах есть картинка чата, но он не отображается сейчас у Вас. Нужно правильно подключать картинки

--- src/Components/ChatsList/template.tsx
+++ src/Components/ChatsList/template.tsx
@@ -3,6 +3,14 @@ export default function template(props: any){
     if(props.list === undefined){
         props.list = []
     }
+    // Если отсутсвует аватар меняет на шаблон
+    props.list.forEach((item: any) => {
+        if(item.avatar === null){
+            item.avatar = "https://sun9-2.userapi.com/impg/Jm6zvW5vic20JtwJZ2LfI6ekmrlD-oxpu3PsLA/51hs3lOtIyE.jpg?size=1051x1080&quality=95&sign=783170df0e41f19239b42e6e2f63bb25&c_uniq_tag=pTQKDny7JUpQrxS_NexcLtAr65IsX8GJtTnOKodR9uk&type=album"
+        }else{
+            item.avatar = `https://ya-praktikum.tech/api/v2/resources/${item.avatar}`
+        }
+    })
     return(
         <div className="chats-chatList">
             {props.list.map((item: any) => {
