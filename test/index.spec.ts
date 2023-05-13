c1e6aba0386d7f2e724f61850283f2da066cc899
ahikyoshi <115409269+ahikyoshi@users.noreply.github.com>
2023-05-13 23:38:24 +0300

    husky test commit

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 // import { expect } from "chai";
 
-
+// asd 
 // describe("Typescript + Babel usage suite", () => {
 //   it("should return string correctly", () => {
 //     expect(hello("mocha"), "Hello mocha");
