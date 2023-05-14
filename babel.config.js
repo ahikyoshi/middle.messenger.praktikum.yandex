b5c112e41dd9eb1c410c5a61a6fa684debdd932a
ahikyoshi <115409269+ahikyoshi@users.noreply.github.com>
2023-05-14 06:01:45 +0300

    fix null error

--- babel.config.js
+++ babel.config.js
@@ -8,16 +8,3 @@ module.exports = {
     '@babel/preset-react'
   ],
 };
-
-
-// module.exports = api => {
-//     // Cache configuration is a required option
-//     api.cache(false);
-  
-//     const presets = [
-//       "@babel/preset-typescript",
-//       "@babel/preset-env"
-//     ];
-  
-//     return { presets };
-//   };
